fc76da20b761d07c63e8d96edf2ce64d44cb9b35
Jang Ryeol <northfolk28@gmail.com>
2017-10-09 19:22:50 +0900

    fix 24 hour millisecond

--- model/admin.ts
+++ model/admin.ts
@@ -1,7 +1,7 @@
 import mongoose = require('mongoose');
 
 export async function getStatistics() {
-    let yesterdayTime = Date.now() - 24 * 3600;
+    let yesterdayTime = Date.now() - 24 * 3600000;
     let userCountPromise = mongoose.connection.db.collection('users').count({});
     let tableCountPromise = mongoose.connection.db.collection('timetables').count({});
     let recentQueryCountPromise = mongoose.connection.db.collection('query_logs')
