9a542dc699025b89b3bfb46342af9833cb0733bc
Jang Ryeol <ryeolj5911@gmail.com>
2017-11-11 17:32:02 +0900

    add pruning logs

--- db.ts
+++ db.ts
@@ -5,28 +5,40 @@ var logger = log4js.getLogger();
 mongoose.Promise = global.Promise;
 
 // connect mongoose
-mongoose.connect(config.mongoUri, function(err) {
-  if(err) {
-    logger.error(err.message);
-    throw err;
-  }
-  if (process.env.NODE_ENV == 'mocha')
-    return;
+async function connect(): Promise<void> {
+  let promise = new Promise(function(resolve, reject){
+    mongoose.connect(config.mongoUri, function(err) {
+      if(err) {
+        reject(err.message);
+      }
+      if (process.env.NODE_ENV == 'mocha')
+        return resolve();
 
-  /**
-   * DB 버전이 2.4 이상인지 확인
-   * 서치 쿼리에서 사용하는 로직이 2.4 이사이어야만 함
-   */
-  var admin = mongoose.connection.db.admin();
-  admin.buildInfo(function (err, info) {
-    if (err) {
-      return logger.error("Could not get mongodb version");
-    }
-    logger.info("MongoDB "+info.version+" connected");
-    if (parseFloat(info.version) < 2.4) {
-      logger.warn("MongoDB version is outdated. (< 2.4) Service might not work properly");
-    }
+      /**
+       * DB 버전이 2.4 이상인지 확인
+       * 서치 쿼리에서 사용하는 로직이 2.4 이사이어야만 함
+       */
+      var admin = mongoose.connection.db.admin();
+      admin.buildInfo(function (err, info) {
+        if (err) {
+          return reject("Could not get mongodb version");
+        }
+        logger.info("MongoDB "+info.version+" connected");
+        if (parseFloat(info.version) < 2.4) {
+          logger.warn("MongoDB version is outdated. (< 2.4) Service might not work properly");
+        }
+        resolve();
+      });
+    });
   });
-});
+
+  try {
+    await promise;
+  } catch (err) {
+    logger.error(err);
+  }
+}
+
+connect();
 
 export = mongoose;
