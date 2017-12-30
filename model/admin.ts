9be6e0338d28d7367a9dcb9faebd2ebc6b0df8ae
Jang Ryeol <ryeolj5911@gmail.com>
2017-12-30 23:27:56 +0900

    admin: add temp user count

--- model/admin.ts
+++ model/admin.ts
@@ -5,11 +5,16 @@ import {getLogFilePath} from '../log/log';
 export async function getStatistics() {
     let yesterdayTime = Date.now() - 24 * 3600000;
     let userCountPromise = mongoose.connection.db.collection('users').count({});
+    let tempUserCountPromise = mongoose.connection.db.collection('users')
+            .count({
+                $and: [{"credential.localId": null}, {"credential.fbId": null}]
+            });
     let tableCountPromise = mongoose.connection.db.collection('timetables').count({});
     let recentQueryCountPromise = mongoose.connection.db.collection('query_logs')
             .count({timestamp: { $gt: yesterdayTime}});
     return {
         userCount: await userCountPromise,
+        tempUserCount: await tempUserCountPromise,
         tableCount: await tableCountPromise,
         recentQueryCount: await recentQueryCountPromise
     }
