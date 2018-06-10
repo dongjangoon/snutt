f2045ced4fc0bb653be0327b094af87a657f3a81
Jang Ryeol <ryeolj5911@gmail.com>
2018-06-10 23:09:56 +0900

    Update mocha dependency #81

--- src/api/test/tag_list_test.ts
+++ src/api/test/tag_list_test.ts
@@ -4,7 +4,7 @@ import errcode = require('core/errcode');
 import { TagList } from 'core/model/tagList';
 
 export = function(app, db, request) {
-  it ('Insert tag lists', async function(done) {
+  it ('Insert tag lists', async function() {
     await TagList.createOrUpdateTags(2015, 1, {
         classification: ['교양'],
         department: ["컴퓨터공학부"],
@@ -13,17 +13,15 @@ export = function(app, db, request) {
         instructor: [ "장병탁" ],
         category: ["인간과 사회"]
     });
-    done();
   });
 
-  it ('Find by semester', async function(done) {
+  it ('Find by semester', async function() {
     let tagList = await TagList.findBySemester(2015, 1);
     assert.equal(tagList.tags.classification[0], '교양');
     assert.equal(tagList.tags.instructor[0], '장병탁');
-    done();
   });
 
-  it ('Update tag lists', async function(done) {
+  it ('Update tag lists', async function() {
     await TagList.createOrUpdateTags(2015, 1, {
         classification: ['교양'],
         department: ["컴퓨터공학부"],
@@ -34,7 +32,6 @@ export = function(app, db, request) {
     });
     let updated = await TagList.findBySemester(2015, 1);
     assert.equal(updated.tags.instructor[0], '문병로');
-    done();
   });
 }
 
