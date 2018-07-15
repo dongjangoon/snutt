183ea27f6f73931d97b44816a3ed87492b6350f4
Jang Ryeol <ryeolj5911@gmail.com>
2018-07-15 16:54:28 +0900

    Fix response string validation error

--- src/core/fcm/FcmLogService.ts
+++ src/core/fcm/FcmLogService.ts
@@ -1,14 +1,14 @@
 import FcmLog from '@app/core/fcm/model/FcmLog';
 import FcmLogRepository = require('@app/core/fcm/FcmLogRepository');
 
-export function addFcmLog(to: string, author: string, message: string, cause: string, response: string): Promise<void> {
+export function addFcmLog(to: string, author: string, message: string, cause: string, response: any): Promise<void> {
     let fcmLog: FcmLog = {
         date: new Date(),
         to: to,
         author: author,
         message: message,
         cause: cause,
-        response: response
+        response: JSON.stringify(response)
     }
     return FcmLogRepository.insertFcmLog(fcmLog);
 }
