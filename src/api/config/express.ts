c3ce84e8fbeffe6ef4d1bfe58c433fc48b186d65
Jang Ryeol <ryeolj5911@gmail.com>
2018-10-21 13:56:04 +0900

    Add static page integration test and fix view dir bug

--- src/api/config/express.ts
+++ src/api/config/express.ts
@@ -15,7 +15,7 @@ var logger = log4js.getLogger();
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '..', 'views'));
 //app.set('view engine', 'jade');
 app.engine('.html', require('ejs').renderFile);
 
