e96d22beb4e8d4a899034f7210fd094506bced23
Jang Ryeol <ryeolj5911@gmail.com>
2019-01-07 14:07:57 +0900

    태그 검색 시에도 결과 캐싱 #96

--- src/core/lecture/RefLectureQueryCacheRepository.ts
+++ src/core/lecture/RefLectureQueryCacheRepository.ts
@@ -1,3 +1,4 @@
+import crypto = require('crypto');
 import RedisKeyUtil = require('@app/core/redis/RedisKeyUtil');
 import RedisUtil = require('@app/core/redis/RedisUtil');
 import log4js = require('log4js');
@@ -5,7 +6,8 @@ import RefLecture from './model/RefLecture';
 let logger = log4js.getLogger();
 
 export async function getListOfLectureListCacheFromPageList(query: any, pageList: number[]): Promise<RefLecture[][] | null> {
-    let keyList = pageList.map(page => RedisKeyUtil.getLectureQueryKey(query, page))
+    let queryHash = makeMd5HashFromObject(query);
+    let keyList = pageList.map(page => RedisKeyUtil.getLectureQueryKey(queryHash, page))
     let lectureListStringList = await RedisUtil.mget(keyList);
     let lectureListList: RefLecture[][] = lectureListStringList.map(parseLectureListString);
     return lectureListList;
@@ -21,7 +23,12 @@ function parseLectureListString(str: string): RefLecture[] | null {
 }
 
 export async function setLectureListCache(query: any, page: number, lectureList: RefLecture[]): Promise<void> {
-    let key = RedisKeyUtil.getLectureQueryKey(query, page);
+    let queryHash = makeMd5HashFromObject(query);
+    let key = RedisKeyUtil.getLectureQueryKey(queryHash, page);
     let lectureListString = JSON.stringify(lectureList);
     await RedisUtil.set(key, lectureListString);
+}
+
+function makeMd5HashFromObject(object: any) {
+    return crypto.createHash('md5').update(JSON.stringify(object)).digest('hex');
 }
--- src/core/redis/RedisKeyUtil.ts
+++ src/core/redis/RedisKeyUtil.ts
@@ -1,10 +1,3 @@
-import crypto = require('crypto');
-
-export function getLectureQueryKey(query: any, page: number) {
-    let queryHash = makeMd5HashFromObject(query);
+export function getLectureQueryKey(queryHash: string, page: number) {
     return "lq-" + queryHash + "-" + page;
-}
-
-function makeMd5HashFromObject(object: any) {
-    return crypto.createHash('md5').update(JSON.stringify(object)).digest('hex');
 }
