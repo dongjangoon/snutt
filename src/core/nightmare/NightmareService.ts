17eae6f0ea4cd5d63443ff078c9acff29ec81f7a
Jang Ryeol <ryeolj5911@gmail.com>
2019-01-27 18:42:52 +0900

    이미지 export 기능 추가 #43

--- src/core/nightmare/NightmareService.ts
+++ src/core/nightmare/NightmareService.ts
@@ -1,10 +1,16 @@
 import Nightmare = require('nightmare');
 import AsyncLock = require('async-lock');
+import log4js = require('log4js');
+let logger = log4js.getLogger();
 
 let nm = Nightmare({
     show: false,
     executionTimeout: 1000,
     waitTimeout: 1000
+});
+
+nm.on('console', function(log, msg) {
+    logger.info("console msg: " + msg);
 });
 
 let nmLock = new AsyncLock({maxPending : 100, timeout: 5000});
@@ -21,9 +27,15 @@ function execute<T>(f: (Nightmare) => Promise<T>): Promise<T> {
 
 export function renderHtmlAsPng(html: string, width: number, height: number): Promise<Buffer> {
     return execute(async function(nm) {
+        let printMessageAfterSecond: any;
         nm.viewport(width, height);
         nm.goto("data:text/html;charset=utf-8," + html);
         await nm.wait(function(){ return true; }); // HTML 전체 로딩까지 대기
+        let result = await nm.evaluate(function(msg) {
+            return printMessageAfterSecond(msg);
+        }, "abcd");
+
+        logger.info("function result: " + result);
         let buffer: Buffer = await nm.screenshot();
         return buffer;
     });
