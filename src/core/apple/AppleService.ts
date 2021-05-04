bddfe8e51dc5d736a9d12e88c6f72c564e32f9db
Hank Choi <zlzlqlzl1@naver.com>
2021-05-04 22:27:59 +0900

    Save codes

--- src/core/apple/AppleService.ts
+++ src/core/apple/AppleService.ts
@@ -8,42 +8,33 @@ import AppleUserInfo from "@app/core/apple/model/AppleUserInfo";
 import * as jwt from "jsonwebtoken";
 import {getMatchedKeyBy} from "@app/core/apple/AppleVerifyUtil";
 import AppleJWK from "@app/core/apple/model/AppleJWK";
-import pj = require('pem-jwk')
 import crypto = require('crypto');
+const jwkToPem = require('jwk-to-pem');
 
 
 
-export function getAppleInfo(identityToken:string): Promise<{appleName:string, appleId:string}> {
-  const headerOfIdentityToken = JSON.parse(Buffer.from(identityToken.substr(0,identityToken.indexOf('.')),'base64').toString());
-  const publicKey: AppleJWK= await getMatchedKeyBy(headerOfIdentityToken.kid,headerOfIdentityToken.alg);
-  jwt.verify(identityToken,JSON.stringify(publicKey))
+export async function getAppleInfo(identityToken: string){
+  const headerOfIdentityToken = JSON.parse(Buffer.from(identityToken.substr(0, identityToken.indexOf('.')), 'base64').toString());
+  const appleJwk: AppleJWK = await getMatchedKeyBy(headerOfIdentityToken.kid, headerOfIdentityToken.alg);
+  const publicKey = jwkToPem(appleJwk);
+  jwt.verify(identityToken, publicKey, {algorithms: [appleJwk.alg],issuer: "https://appleid.apple.com",audience: "9RQ8S5ZACJ"});
 
-  const userInfo: AppleUserInfo = jwt.decode(identityToken);
-  return new Promise(function(resolve, reject) {
-    request({
-      url: "https://appleid.apple.com/auth/token",
-      method: "GET",
-      json: true,
-      qs: {access_token: appleToken}
-    }, function (err, res, body) {
-      if (err || res.statusCode != 200 || !body || !body.id || fbId !== body.id) {
-        return reject(new InvalidFbIdOrTokenError(fbId, fbToken));
-      } else {
-        return resolve({fbName: body.name, fbId: body.id});
-      }
-    });
-  });
+  // const userInfo: AppleUserInfo = jwt.decode(identityToken);
+  // return new Promise(function (resolve, reject) {
+  //   request({
+  //     url: "https://appleid.apple.com/auth/token",
+  //     method: "GET",
+  //     json: true,
+  //     qs: {access_token: appleToken}
+  //   }, function (err, res, body) {
+  //     if (err || res.statusCode != 200 || !body || !body.id || fbId !== body.id) {
+  //       return reject(new InvalidFbIdOrTokenError(fbId, fbToken));
+  //     } else {
+  //       return resolve({fbName: body.name, fbId: body.id});
+  //     }
+  //   });
+  // });
 }
 
-export function decodeAppleIdentityToken(token): AppleUserInfo {
-  const appleUserInfo:AppleUserInfo = jwt.decode(token)
-  return new Promise(function(resolve, reject) {
-    request({
-      url: "https://appleid.apple.com/auth/token",
-      method: "GET",
-      json: true,
-      qs: {access_token: appleToken}
-    }, function (err, res, body) {
-
-    }
+export function decodeAppleIdentityToken(token) {
 }
