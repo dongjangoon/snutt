da2ef874e754f1c71938bc4dac5706f5ed2c15b5
sanggggg <ksme6776@gmail.com>
2021-05-27 23:08:32 +0900

    ValidationPipe 추가

--- src/app/cats/cats.controller.ts
+++ src/app/cats/cats.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common'
+import { Body, Controller, Get, Post } from '@nestjs/common'
 import { CatsService } from './cats.service'
 import { Cat } from '@snutt-schema/cat.schema'
+import { CatsDto } from '@snutt-app/cats/cats.dto'
 
 @Controller('cats')
 export class CatsController {
@@ -9,5 +10,10 @@ export class CatsController {
   @Get()
   getCat(): Promise<Cat> {
     return this.catsService.create({ name: 'asdf', age: 1, breed: 'asdf' })
+  }
+
+  @Post('/test')
+  getTest(@Body() params: CatsDto) {
+    return params.name.repeat(params.count)
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app/app.module'
 import { SNUTTExceptionFilter } from './middleware/exception/exception.filter'
+import { ValidationPipe } from '@nestjs/common'
+import { ApiInvalidInputError } from '@snutt-middleware/exception/api-error'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: (e) => {
+        console.error(e)
+        return new ApiInvalidInputError()
+      },
+    }),
+  )
   app.useGlobalFilters(new SNUTTExceptionFilter())
   await app.listen(3000)
 }
